82a006ff06e7e6586515676963fc55db7385b30d
Rafael Gomes Pinheiro <61011140+RafaelGomes00@users.noreply.github.com>
2025-05-27 17:32:18 -0300

    Compress resume

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,8 +21,8 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> Rafael Gomes </strong>
+                I'm
+                <strong className="main-name"> Rafael </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
