a8b1b71af2b9f78c719afccc3c9fee2826e1488b
Rafael Gomes Pinheiro <61011140+RafaelGomes00@users.noreply.github.com>
2025-05-30 16:50:28 -0300

    Update resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -32,7 +32,13 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Row className="resume" style={{ justifyContent: "center" }}>
+        <Row className="resume">
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </Row>
+
+        {/* <Row className="resume" style={{ justifyContent: "center" }}>
           <img
             src={resumeImage}
             alt="Rafael Gomes Resume"
@@ -43,7 +49,7 @@ function ResumeNew() {
               boxShadow: "0 0 10px rgba(0,0,0,0.2)",
             }}
           />
-        </Row>
+        </Row> */}
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
